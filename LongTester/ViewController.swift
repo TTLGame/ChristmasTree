5b0e3f01e269bb9cc63f5746a2a4623792fd1074
Long Pro <longtt@unit.com.vn>
2022-12-28 10:36:10 +0700

    Initial Commit

---
+++ LongTester/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LongTester
+//
+//  Created by Long on 12/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
